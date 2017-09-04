2e852b405f0566c12b1cf030c463f64a72f4892e
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2017-09-04 17:47:45 -0400

    Added a check for contacts, and displaying it

--- datacombine/datacombine/views.py
+++ datacombine/datacombine/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import View
 from django.shortcuts import render
+from datacombine import models
 
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home.html')
+        return render(
+            request,
+            'home.html',
+            {'contacts_count': models.Contact.objects.count()}
+        )
